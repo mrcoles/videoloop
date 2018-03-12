cd792d9c62ba0482dab39fc839af0e3e57d7f67f
Peter Coles <peter@mrcoles.com>
2018-03-11 22:18:24 -0400

    got full screen working
    still need to full screen the browser, but I think thats ok

--- src/js/index.js
+++ src/js/index.js
@@ -6,21 +6,72 @@ window._$ = $; //REM
 
 let DELAY = 1000;
 let NUM_CANVASES = 1;
-const WIDTH = 500;
-const HEIGHT = 375;
+const DEFAULT_WIDTH = 500;
+const DEFAULT_HEIGHT = 375;
+
+let CUR_WIDTH = DEFAULT_WIDTH;
+let CUR_HEIGHT = DEFAULT_HEIGHT;
 
 const _create = tag => document.createElement(tag);
-const _sizeElt = elt => Object.assign(elt, { width: WIDTH, height: HEIGHT });
+const _sizeElt = elt =>
+  Object.assign(elt, { width: CUR_WIDTH, height: CUR_HEIGHT });
 const _makeCanvas = () =>
   Object.assign(_sizeElt(_create('canvas')), { className: 'canvas' });
 
 // ## Canvas
+
+const CLS_NORMAL = 'normal';
+const CLS_SEPARATE = 'separate';
+const CLS_FULLSCREEN = 'fullscreen';
+
+const CLS_NAMES = [CLS_NORMAL, CLS_SEPARATE, CLS_FULLSCREEN];
 
 const Layers = {
   video: _sizeElt(document.getElementById('video')),
   canvases: [],
   ctxs: [],
   transforms: [],
+  curClsName: CLS_NORMAL,
+  show: clsName => {
+    if (CLS_NAMES.indexOf(clsName) === -1) {
+      throw new Error(`Unknown class name: ${clsName}`);
+    }
+    Layers.curClsName = clsName;
+
+    CLS_NAMES.forEach(cls => document.body.classList.remove(cls));
+    if (clsName) {
+      document.body.classList.add(clsName);
+    }
+
+    if (clsName === CLS_FULLSCREEN) {
+      let width = window.innerWidth;
+      let video = Layers.video;
+      let height = video.videoHeight / video.videoWidth * width;
+
+      CUR_WIDTH = width;
+      CUR_HEIGHT = height;
+    } else {
+      CUR_WIDTH = DEFAULT_WIDTH;
+      CUR_HEIGHT = DEFAULT_HEIGHT;
+    }
+
+    Layers._resizeElts();
+  },
+  _resizeElts: () => {
+    [[Layers.video], Layers.canvases].forEach(elts =>
+      elts.forEach(elt => {
+        elt.setAttribute('width', CUR_WIDTH);
+        elt.setAttribute('height', CUR_HEIGHT);
+        elt.style.width = `${CUR_WIDTH}px`;
+        elt.style.height = `${CUR_HEIGHT}px`;
+      })
+    );
+  },
+  showNext: () => {
+    let curIndex = CLS_NAMES.indexOf(Layers.curClsName);
+    let nextClsName = CLS_NAMES[curIndex + 1] || CLS_NAMES[0];
+    Layers.show(nextClsName);
+  },
   _nextId: 0,
   _getNextId: () => {
     Layers._nextId++;
@@ -89,25 +140,31 @@ promUserMedia({ video: true })
 
 const _queueCanvas = () => {
   let tCanvas = _sizeElt(_create('canvas'));
-  tCanvas.getContext('2d').drawImage(Layers.video, 0, 0, WIDTH, HEIGHT);
+  tCanvas.getContext('2d').drawImage(Layers.video, 0, 0, CUR_WIDTH, CUR_HEIGHT);
   Manip.transform(Layers.transforms, tCanvas);
 
   Layers.ctxs.forEach((ctx, i) => {
     window.setTimeout(() => {
       window.requestAnimationFrame(() => {
-        ctx.clearRect(0, 0, WIDTH, HEIGHT);
-        ctx.drawImage(tCanvas, 0, 0, WIDTH, HEIGHT);
+        ctx.clearRect(0, 0, CUR_WIDTH, CUR_HEIGHT);
+        ctx.drawImage(tCanvas, 0, 0, CUR_WIDTH, CUR_HEIGHT);
       });
     }, DELAY * (i + 1));
   });
 };
 
 const _drawCanvas = () =>
-  Layers.ctxs[0].drawImage(Layers.video, 0, 0, WIDTH, HEIGHT);
+  Layers.ctxs[0].drawImage(Layers.video, 0, 0, CUR_WIDTH, CUR_HEIGHT);
 
 // ## Controls
 
 (function() {
+  $('#display').on('click', function() {
+    if (Layers.curClsName === CLS_FULLSCREEN) {
+      Layers.showNext();
+    }
+  });
+
   $('#id_delay')
     .on('change keyup', function() {
       let text = $(this).val();
@@ -148,16 +205,16 @@ const _drawCanvas = () =>
     Layers.transforms = names.map(name => Manip.fn[name]);
   });
 
-  $('#id_separate').on('click', function(e) {
-    document.body.classList.toggle('separate');
+  $('#id_changedisplay').on('click', function(e) {
+    Layers.showNext();
   });
 
   $('#id_setbg').on('click', function(e) {
     let bgCanvas = _sizeElt(_create('canvas'));
     let bgCtx = bgCanvas.getContext('2d');
-    bgCtx.drawImage(video, 0, 0, WIDTH, HEIGHT);
+    bgCtx.drawImage(video, 0, 0, CUR_WIDTH, CUR_HEIGHT);
     // Layers.bgCanvas = Manip.transform([Manip.fn.invert], bgCanvas);
-    Layers.bgData = bgCtx.getImageData(0, 0, WIDTH, HEIGHT).data;
+    Layers.bgData = bgCtx.getImageData(0, 0, CUR_WIDTH, CUR_HEIGHT).data;
     $('#bg-canvas').remove();
     bgCanvas.id = 'bg-canvas';
     bgCanvas.classList.add('canvas');
@@ -173,7 +230,7 @@ const Manip = {
     }
     let imageData = fromCanvas
       .getContext('2d')
-      .getImageData(0, 0, WIDTH, HEIGHT);
+      .getImageData(0, 0, CUR_WIDTH, CUR_HEIGHT);
     transforms.forEach(transform => transform(imageData.data));
     toCanvas.getContext('2d').putImageData(imageData, 0, 0);
     return toCanvas;
